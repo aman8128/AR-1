8297925c3f96074f7a29ae60182e124bcfd9503b
aman8128 <c6412702@gmail.com>
2025-09-13 16:30:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SneakerAR from './components/SneakerAR';
+
+function App() {
+  return (
+    <div className="App">
+      <SneakerAR />
+    </div>
+  );
+}
+
+export default App;
